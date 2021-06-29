9cc2bdbf778bb52acdbda713427c79c4238eec1b
Christiano0407 <placebo0704@gmail.com>
2021-06-28 21:44:10 -0500

    figuresTwo

---
+++ src/js/figureApp.js
@@ -0,0 +1,14 @@
+console.log("Figure"); 
+// Figuras Geométricas
+
+// Parámetro / área / Radio / Diámetro.
+
+const ladoCuadrado = 5; 
+console.log(ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4; 
+console.log(perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado / 2; 
+console.log(`Es el área total: ${areaCuadrado}cm`);
+

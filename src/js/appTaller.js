3a3257fd46b23b75ec3b0ee34b70563298f4a491
Christiano0407 <placebo0704@gmail.com>
2021-07-02 09:09:49 -0500

    precios con variables

--- src/js/appTaller.js
+++ src/js/appTaller.js
@@ -1,1 +1,33 @@
 console.log("Two taller"); 
+// ================ PRECIO / DESCUENTOS / Porcentajes / Ventas =================
+
+// Primer paso: Definir fórmula
+     // Precio * (100 - %) / 100    // => % = Descuento.
+
+// Segundo: Implementar la fórmula en javascript.
+// Tercer: Crear las funciones.
+// Integrar HTML a Javascipt.
+
+// ====== 1) Nuestras Variables: ========
+console.group("Variables"); 
+const precioOriginal = 150; 
+
+const descuento = 20; 
+// Descuento en porcentaje = 15%
+
+const porcentajePrecioConDescuento = 100 - descuento;
+//console.log(procentajePrecioConDescuento); 
+const precioFijo = (precioOriginal * porcentajePrecioConDescuento) / 100; 
+//console.log(`El precio total a pagar: ${precioFijo} pesos`);
+
+console.log({
+     precioOriginal, 
+     descuento,
+     porcentajePrecioConDescuento, 
+     precioFijo,
+}); 
+
+
+console.groupEnd(); 
+
+// ======= Lo vamos a cambiar a una función =======

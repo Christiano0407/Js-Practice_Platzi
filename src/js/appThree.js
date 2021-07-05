8c7f7ab6dbb2afc160f9cea019cf6670e6efae70
Christiano0407 <placebo0704@gmail.com>
2021-07-05 08:21:53 -0500

    variables

---
+++ src/js/appThree.js
@@ -0,0 +1,22 @@
+console.log("Practice Two / Taller Dos"); 
+// ===========Variables ===========>
+const inputPrice = document.getElementById(`inputPrice`); 
+
+const inputDiscount = document.getElementById(`inputDiscount`); 
+
+const money = document.getElementById(`money`); 
+
+const button = document.getElementById(`btn`); 
+
+// ==================Funciones / Eventos ===========>
+console.group("Practica Dos"); 
+
+console.log(money); 
+
+button.addEventListener(`click`, () => {
+    console.log("Click Btn"); 
+
+}); 
+
+
+console.groupEnd(); 

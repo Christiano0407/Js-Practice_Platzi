a28088700aea1532d69f22391e7b6100117f74e7
Christiano0407 <placebo0704@gmail.com>
2021-07-05 09:10:39 -0500

    button/event

--- src/js/appThree.js
+++ src/js/appThree.js
@@ -1,10 +1,10 @@
 console.log("Practice Two / Taller Dos"); 
 // ===========Variables ===========>
-const inputPrice = document.getElementById(`inputPrice`); 
+/* const inputPrice = document.getElementById(`inputPrice`);  */
 
-const inputDiscount = document.getElementById(`inputDiscount`); 
+/* const inputDiscount = document.getElementById(`inputDiscount`);  */
 
-const money = document.getElementById(`money`); 
+/* const money = document.getElementById(`money`);  */
 
 const button = document.getElementById(`btn`); 
 
@@ -12,11 +12,31 @@ const button = document.getElementById(`btn`);
 console.group("Practica Dos"); 
 
 console.log(money); 
+// Funcción para llamar Precio con Descuento ====>
+calcularPrecioConDescuento = (precio, descuento) => {
 
+    const porcentajePrecioConDescuento = 100 - descuento; 
+
+    let precioPago = (precio * porcentajePrecioConDescuento) / 100; 
+    return precioPago; 
+}
+console.log(calcularPrecioConDescuento(100, 15)); 
+
+// Evento  =========>
 button.addEventListener(`click`, () => {
     console.log("Click Btn"); 
 
-}); 
+    const inputPrice = document.getElementById(`inputPrice`); 
+    const priceValue = inputPrice.value; 
 
+    const inputDiscount = document.getElementById(`inputDiscount`);
+    const discountValue = inputDiscount.value;
+    
+    const precioTotal = calcularPrecioConDescuento(priceValue, discountValue); 
+    //console.log(`El precio Total es: ${precioTotal}`); 
+    const money = document.getElementById(`money`);
+    money.innerText = `El monto total a pagar:$ ${precioTotal} dólares`; 
+
+}); 
 
 console.groupEnd(); 

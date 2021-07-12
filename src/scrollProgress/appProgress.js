b1a7abb26d1195312cc5bbd4d3f8299b18c50b86
Christiano0407 <placebo0704@gmail.com>
2021-07-12 12:44:21 -0500

    final

--- src/scrollProgress/appProgress.js
+++ src/scrollProgress/appProgress.js
@@ -7,16 +7,28 @@ let progressBar = document.querySelector(`.progress-bar-wrap`);
 let progressNum = document.querySelector(`.progress-num`); 
 let x, y; 
 // ========= Functions / Porcentaje / Scroll ============ > 
+
+// Activar el mover scrollSection === > 
+/* window.addEventListener(`mouseover`, (e) => { */
+/*     x = e.clientX;  */
+/*     y = e.clientY;  */
+/* });  */
+
+// Llamar scroll en altura y porcentaje > 
 updateProgressBar = () => {
  /*   console.log(getScrollPercentage());  */
+
+   //progressSection.style.transform = `translate(${x}px, ${y}px)`; 
    progressBar.style.height = `${getScrollPercentage()}%`; 
+   progressNum.innerText = `${Math.ceil(getScrollPercentage())}%`; 
    requestAnimationFrame(updateProgressBar); 
-}
+
+}; 
 
 getScrollPercentage = () => {
     return((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100);
 
-}
+}; 
 
 
 updateProgressBar(); 

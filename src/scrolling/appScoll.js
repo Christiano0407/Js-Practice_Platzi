4381a91dcfe2539fe641b72a086f7713d7d5cd6d
Christiano0407 <placebo0704@gmail.com>
2021-07-12 11:26:17 -0500

    scrolling Progress Indicator

--- src/scrolling/appScoll.js
+++ src/scrolling/appScoll.js
@@ -19,6 +19,22 @@ function lerp(start, end, t) {
 
 function setTransform(el, transform) {
     el.style.transform = transform; 
+}; 
+
+function init(){
+    sliderWidth = slider.getBoundingClientRect().width; 
+    imageWidth = sliderWidth / images.length;
+    document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`;
+}; 
+
+function animate() {
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY; 
+    setTransform(slider, `translateX(-${current}px)`); 
+    requestAnimationFrame(animate);  
 }
 
+init(); 
+animate(); 
+console.log(imageWidth); 
 console.groupEnd(); 

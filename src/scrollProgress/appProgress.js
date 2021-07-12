02f08940a70493ddd6c884c662d929a4af31fe9a
Christiano0407 <placebo0704@gmail.com>
2021-07-12 12:26:55 -0500

    precentage

--- src/scrollProgress/appProgress.js
+++ src/scrollProgress/appProgress.js
@@ -1,1 +1,23 @@
 console.log("Progress"); 
+
+console.group("Scroll Progress"); 
+// Varisbles =========== > 
+let progressSection = document.querySelector(`.progress-section`); 
+let progressBar = document.querySelector(`.progress-bar-wrap`); 
+let progressNum = document.querySelector(`.progress-num`); 
+let x, y; 
+// ========= Functions / Porcentaje / Scroll ============ > 
+updateProgressBar = () => {
+ /*   console.log(getScrollPercentage());  */
+   progressBar.style.height = `${getScrollPercentage()}%`; 
+   requestAnimationFrame(updateProgressBar); 
+}
+
+getScrollPercentage = () => {
+    return((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100);
+
+}
+
+
+updateProgressBar(); 
+console.groupEnd(); 
